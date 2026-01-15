cda9dad54cab95090caee423c011b187c61cf813
maudesoul <shaffertaniya@gmail.com>
2026-01-15 11:57:56 -0800

    removed tracked next build artifacts from remote branch

--- .next/dev/static/chunks/src_app_layout_tsx_78cdd4a3._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_e529b4dc._.js",
-  "static/chunks/src_app_e341d96c._.js"
-],
-    source: "dynamic"
-});
